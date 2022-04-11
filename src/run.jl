d27230a4f19af4b6a4f4ebe39dbba37534f7a519
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-04-11 20:38:19 -0300

    :wrench: Add function to execute a Monte Carlo simulation

--- src/run.jl
+++ src/run.jl
@@ -7,6 +7,38 @@
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+function monte_carlo(number_of_runs::Int, number_of_epochs::Int; kwargs...)
+    results = Vector{DataFrame}(undef, number_of_runs)
+
+    for k in 1:number_of_runs
+        @info "Run #$k..."
+        model = initialize_model(; kwargs...)
+
+        adata = nothing
+        mdata = nothing
+
+        for _ in 1:number_of_epochs
+            adata, mdata = run_simulation(model)
+        end
+
+        results[k] = adata
+    end
+
+    # Compute the values.
+    step  = first(results).step
+    total = round.(Int, mapreduce(x -> x.total, +, results) ./ number_of_runs)
+    left  = round.(Int, mapreduce(x -> x.left,  +, results) ./ number_of_runs)
+
+    # Compute the DataFrame with the result.
+    df = DataFrame(
+        step  = step,
+        total = total,
+        left  = left,
+    )
+
+    return df
+end
+
 function run_simulation(model)
     adf, mdf = run!(
         model,
@@ -27,7 +59,7 @@ function run_simulation(model)
 end
 
 function terminate(model, step)
-    if (step > 1000) || (length(allagents(model)) == 0)
+    if (step ≥ model.properties.number_of_steps) || (length(allagents(model)) == 0)
         return true
     else
         return false
